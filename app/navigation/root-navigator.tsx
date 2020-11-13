1512f0a3e01e04745e6d746b6f3fcf8815c329fd
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-13 12:06:25 +0200

    feat: added StaticNavigator

--- app/navigation/root-navigator.tsx
+++ app/navigation/root-navigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import { InitialScreen, HomeScreen } from '../screens';
+import { navigationRef } from '../services/navigator';
 
 type RootParams = {
   Initial: undefined;
@@ -11,7 +12,7 @@ type RootParams = {
 const Stack = createNativeStackNavigator<RootParams>();
 
 const RootNavigator = () => (
-  <NavigationContainer>
+  <NavigationContainer ref={navigationRef}>
     <Stack.Navigator>
       <Stack.Screen name="Initial" component={InitialScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
